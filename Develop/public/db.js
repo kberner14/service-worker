f8911fd03a4890aa6e293c98f0b26a05e4225c1b
Kyle Berner <kyleberner14@yahoo.com>
2020-05-05 21:23:09 -0700

    created the saveRecord function for saving an object while offline

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -8,16 +8,16 @@ window.msIndexedDB ||
 window.shimIndexedDB;
 
 let db;
-const request = indexedDB.open("budget", 1);
+const request = window.indexedDB.open("budget", 1);
 
 // TODO: create an object store in the open db
 request.onupgradeneeded = function(event) {
   // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+  db.createObjectStore("pending", { keyPath: "id", autoIncrement: true });
 };
-request.onsuccess = function(event) {
-  db = event.target.result;
+request.onsuccess = function({ target }) {
+  db = target.result;
   
   if (navigator.onLine) {
     checkDatabase();
@@ -26,16 +26,20 @@ request.onsuccess = function(event) {
 
 // TODO: log any indexedDB errors
 request.onerror = function(event) {
-  console.log(`Woops! ${ event.target.errorCode}`);
+  console.log(event.error);
 };
 
-// TODO: add code so that any transactions stored in the db
-// are sent to the backend if/when the user goes online
-// Hint: learn about "navigator.onLine" and the "online" window event.
-
-// TODO: add code to saveRecord so that it accepts a record object for a
-// transaction and saves it in the db. This function is called in index.js
-// when the user creates a transaction while offline.
-function saveRecord(record) {
-  // add your code here
+  
+  // TODO: add code so that any transactions stored in the db
+  // are sent to the backend if/when the user goes online
+  // Hint: learn about "navigator.onLine" and the "online" window event.
+  
+  // TODO: add code to saveRecord so that it accepts a record object for a
+  // transaction and saves it in the db. This function is called in index.js
+  // when the user creates a transaction while offline.
+  function saveRecord(record) {
+    // add your code here
+    const tx = db.transaction(["pending"], "readwrite");
+    const pendingStore = tx.objectStore("pending");
+    pendingStore.add(record);
 }
