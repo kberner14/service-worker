a7d47076b0529ea516437b0d0c2a9affa1789e83
Kyle Berner <kyleberner14@yahoo.com>
2020-05-05 23:50:27 -0700

    updated icon sizes based on errors. Actual sizes were incorrect

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/index.js",
-  "/manifest.json",
-  "/db.js",
-  "/public/icons/icon-192x192.png",
-  "/public/icons/icon-512x512.png",
-  "/style.css",
+  "./",
+  "./index.html",
+  "./index.js",
+  "./manifest.json",
+  "./db.js",
+  "./public/icons/icon-192x192.png",
+  "./public/icons/icon-512x512.png",
+  "./style.css",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
